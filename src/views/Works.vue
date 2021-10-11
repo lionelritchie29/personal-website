774d7c12b94c8074227264957e2ff40509acc008
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-10-11 19:24:47 +0700

    Added another works

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -22,10 +22,10 @@
 </template>
 
 <script>
-import PortfolioItem from "../components/portfolio/items/PortItem";
+import PortfolioItem from '../components/portfolio/items/PortItem';
 
 export default {
-  name: "works",
+  name: 'works',
   components: {
     PortfolioItem,
   },
@@ -34,63 +34,82 @@ export default {
       portfolio: [
         {
           id: 1,
-          name: "Foodie",
+          name: 'Foodie',
           desc:
-            "Foodie, as the name implies, is a simple web app that provide information about food. Features consist of showing random recipe, search for a specific recipe, generate a meal plan, and generate random trivia and joke. Data are provided by Spoonacular Food API.",
-          url: "foodie.jpg",
-          position: "left",
-          style: "md:ml-12 md:mr-40",
-          link: "https://dicoding-foodie.netlify.app/",
+            'Foodie, as the name implies, is a simple web app that provide information about food. Features consist of showing random recipe, search for a specific recipe, generate a meal plan, and generate random trivia and joke. Data are provided by Spoonacular Food API.',
+          url: 'foodie.jpg',
+          position: 'left',
+          style: 'md:ml-12 md:mr-40',
+          link: 'https://dicoding-foodie.netlify.app/',
         },
         {
           id: 2,
-          name: "FinTalks",
+          name: 'FinTalks',
           desc:
-            "This is a Progressive Web Application (PWA), meaning it could act like a native mobile application with features such as add to home screen and some/all features still could be used while offline as long as the page(s) is already visited once while online. FinTalks is a fictional independent financial advisor company in Indonesia. ",
-          url: "fintalks.jpg",
-          position: "right",
-          style: "md:mr-12 md:ml-40",
-          link: "https://fin-talks.web.app/",
+            'This is a Progressive Web Application (PWA), meaning it could act like a native mobile application with features such as add to home screen and some/all features still could be used while offline as long as the page(s) is already visited once while online. FinTalks is a fictional independent financial advisor company in Indonesia. ',
+          url: 'fintalks.jpg',
+          position: 'right',
+          style: 'md:mr-12 md:ml-40',
+          link: 'https://fin-talks.web.app/',
         },
         {
           id: 3,
-          name: "Football Spa",
+          name: 'Football Spa',
           desc:
-            "FootballSpa (Football Spain) is a non-profit site that display matches, teams, and standings of all football teams from Spain. Football data provided by the Football-Data.org API. Features consist of show upcoming matches, show all teams, show current standings, show team detail, and add to favorite (and also show notification after that!). This web app also uses Progressive Web Apps (PWA) aprroach, meaning it could act like a native mobile application (add to home screen, show notification, used while offline).",
-          url: "football-spa.jpg",
-          position: "left",
-          style: "md:ml-12 md:mr-40",
-          link: "https://football-spa.web.app/",
+            'FootballSpa (Football Spain) is a non-profit site that display matches, teams, and standings of all football teams from Spain. Football data provided by the Football-Data.org API. Features consist of show upcoming matches, show all teams, show current standings, show team detail, and add to favorite (and also show notification after that!). This web app also uses Progressive Web Apps (PWA) aprroach, meaning it could act like a native mobile application (add to home screen, show notification, used while offline).',
+          url: 'football-spa.jpg',
+          position: 'left',
+          style: 'md:ml-12 md:mr-40',
+          link: 'https://football-spa.web.app/',
         },
         {
           id: 4,
-          name: "Restoqu",
+          name: 'Restoqu',
           desc:
             "Restoqu is an app that I create for Dicoding's final class on Front End Web Developer Learning Path (Being an expert front-end web developer) submission. The app provides information about restaurants and their detail. The app implements modern web technology features such as Progressive Web Apps,  indexed DB, and native web components.",
-          url: "restoqu.jpg",
-          position: "right",
-          style: "md:mr-12 md:ml-40",
-          link: "https://restoqu.netlify.app",
+          url: 'restoqu.jpg',
+          position: 'right',
+          style: 'md:mr-12 md:ml-40',
+          link: 'https://restoqu.netlify.app',
         },
         {
           id: 5,
-          name: "Staem (Steam clone)",
+          name: 'Staem (Steam clone)',
           desc:
-            "Steam clone for TPA (Test Progressive of Assistant) Web as a teaching assistant at Binus University. Available features: Login, Register (Sending OTP Email), Homepage, Game Detail (Info and Review), Search Game, Friends (Add, View, Pending Invites), Real-time chat, Redeem Wallet, Profile, Edit Profile, Points Shop, Community, Admin (Game, User, Promo), Inventory, Buy Game (Self), Cart System, Wishlist, and Market (Half). Front-end created with Angular, SCSS, and Apollo. Backend built using Postgresql, Go and Graphql. Credentials you can use: lionelritchie (account name) and lionel123 (password)",
-          url: "staem.jpg",
-          position: "left",
-          style: "md:ml-12 md:mr-40",
-          link: "https://staem.netlify.app",
+            'Steam clone for TPA (Test Progressive of Assistant) Web as a teaching assistant at Binus University. Available features: Login, Register (Sending OTP Email), Homepage, Game Detail (Info and Review), Search Game, Friends (Add, View, Pending Invites), Real-time chat, Redeem Wallet, Profile, Edit Profile, Points Shop, Community, Admin (Game, User, Promo), Inventory, Buy Game (Self), Cart System, Wishlist, and Market (Half). Front-end created with Angular, SCSS, and Apollo. Backend built using Postgresql, Go and Graphql. Credentials you can use: lionelritchie (account name) and lionel123 (password)',
+          url: 'staem.jpg',
+          position: 'left',
+          style: 'md:ml-12 md:mr-40',
+          link: 'https://staem.netlify.app',
         },
         {
           id: 6,
-          name: "Gojek Homepage Clone",
+          name: 'Gojek Homepage Clone',
           desc:
             "I created this not so-clone of Gojek's homepage to learn React.",
-          url: "gojek.jpg",
-          position: "right",
-          style: "md:mr-12 md:ml-40",
-          link: "https://gojek.netlify.app/",
+          url: 'gojek.jpg',
+          position: 'right',
+          style: 'md:mr-12 md:ml-40',
+          link: 'https://gojek.netlify.app/',
+        },
+        {
+          id: 7,
+          name: 'Pokedex',
+          desc: 'Simple Pokedex created with React',
+          url: 'pokedex.jpg',
+          position: 'left',
+          style: 'md:ml-12 md:mr-40',
+          link: 'https://react-graphql-pokedex.netlify.app/',
+        },
+        {
+          id: 8,
+          name: 'Store Del Luna',
+          desc:
+            'I created this to fullfil the final project for Human and Computer Interaction course, which was to create a website about K-Pop Album and Merchandise Shop',
+          url: 'store-del-luna.jpg',
+          position: 'right',
+          style: 'md:mr-12 md:ml-40',
+          link: 'https://final-project-hci.netlify.app/',
         },
       ],
     };
